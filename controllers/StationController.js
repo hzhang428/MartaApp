083c9240450e881752f7a026053752b12fcc02cd
haozhang <hzhang428@gatech.edu>
2017-12-12 16:07:08 -0500

    Get station by id method

--- controllers/StationController.js
+++ controllers/StationController.js
@@ -5,7 +5,6 @@ module.exports = {
         getConnecton(function(err, con) {
             if (err) {
                 callback(err, null);
-                return;
             } else {
                 var sql = 'SELECT * FROM Station';
                 con.query(sql, function(err, Stations) {
@@ -14,5 +13,31 @@ module.exports = {
                 });
             }
         });
+    },
+
+    findById: function(params, callback) {
+        getConnecton(function(err, con) {
+            if (err) {
+                callback(err, null);
+            } else {
+                // console.log(params);
+                var columns = []
+                var sql = 'SELECT ' + 
+                          'A.StopID, A.Name, A.EnterFare, A.IsTrain, A.Status, B.Intersection ' +
+                          'FROM ' + 
+                          'Station AS A ' + 
+                          'LEFT JOIN ' + 
+                          'BusStationIntersection AS B ' + 
+                          'ON ' + 
+                          'A.StopID = B.StopID ' + 
+                          'WHERE ' + 
+                          'A.StopID = ?';
+                con.query(sql, [params], function(err, Station) {
+                    // console.log(Station);
+                    callback(null, Station);
+                    con.release();
+                });
+            }
+        });
     }
-}
+};
--- routes/api.js
+++ routes/api.js
@@ -17,9 +17,29 @@ router.get('/:resource', function(req, res, next) {
                     message: results
                 });
             }
-        })
-
+        });
     } 
 });
+
+router.get('/:resource/:stopID', function(req, res, next) {
+    var resource = req.params.resource;
+    var stopID = req.params.stopID;
+    
+    if (resource == 'station') {
+        StationController.findById(stopID, function(err, result) {
+            if (err) {
+                res.json({
+                    confirmation: "fail",
+                    message: err
+                })
+            } else {
+                res.json({
+                    confirmation: "success",
+                    message: result
+                });
+            }
+        });
+    } 
+})
 
 module.exports = router
